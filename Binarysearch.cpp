8bb2931cb96fc0dc936120a3a90c24d6b5b0169d
Nikhil Sharma <nikhilsharma1552000@gmail.com>
2024-06-22 18:31:21 +0530

    class Addeed

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
  using namespace std;
+ class Nikhil{
+private: int id;
+ string name;
+
+ };
  int main(){
   cout<<"main";
   cout<<"person";
   cout<<"name:Nikhil \t Lastname : Sharma";
   cout<<" git  Commands";
+
   return 0;
  }
  
