c570eaf7684792202d61e639b5fa55638c71ea74
Nikhil Sharma <nikhilsharma1552000@gmail.com>
2024-06-29 23:40:32 +0530

    Namspace  added

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,9 +1,10 @@
  #include<iostream>
-  struct student{
+ using  namespace std;
+ struct student{
 
   };
- int main(){
-  int a = 10;
-  float  d =23.4;
-  cout<<"Value of  a  is " <<a ;
+int main(){
+int a = 10;
+float  d =23.4;
+cout<<"Value of  a  is "<<a ;
  }
